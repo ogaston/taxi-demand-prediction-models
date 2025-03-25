ac5201d03d88aa18e98a750b8862161c2c2f48cb
MaestroOso <oscarjcr95@gmail.com>
2025-03-25 12:08:45 -0500

    Test NeuralNetwork

---
+++ NeuralNetwork/TrainedNeuralNetwork.py
@@ -0,0 +1,15 @@
+import joblib
+import numpy as np
+from tensorflow import keras
+
+model_storage_location = '../Data/taxi_model.keras'
+model = keras.models.load_model(model_storage_location)
+
+scaler_storage_location = '../Data/scaler.pkl'
+scaler = joblib.load(scaler_storage_location)
+
+# Predict
+sample_location = np.array([['-73.990', '40.750', '2015', '01', '15', '19']])
+sample_location_scaled = scaler.transform(sample_location)
+predicted_taxis = model.predict(sample_location_scaled)
+print(f"Predicted number of taxis: {predicted_taxis[0][0]}")
